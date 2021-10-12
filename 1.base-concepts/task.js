d1d4d663579e0b108efacd217a6396c1ea8fecb1
Yuliya Trunova <trunovanova@gmail.com>
2021-10-12 16:02:47 +0300

    changes

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,16 +1,14 @@
 "use strict"
 function solveEquation(a, b, c) {
   let arr = [];
-  let D = b**2 - 4*a*c;
+  let d = b**2 - 4*a*c;
   let x1; 
   let x2;
 
-  if (D < 0) {
-    arr = [];
-  } else if (D == 0) {
+  if (d == 0) {
     x1 = -b / (2 * a)
     arr.push(x1);
-  } else if (D > 0) {
+  } else if (d > 0) {
     x1 = (-b + Math.sqrt(d))/(2*a);
     x2 = (-b - Math.sqrt(d) )/(2 * a);
     arr.push(x1, x2);
