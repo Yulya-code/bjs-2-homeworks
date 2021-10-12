68f3f9dff4b3ce68b807945a2f1d5aebece7492a
Yuliya Trunova <trunovanova@gmail.com>
2021-10-12 14:27:54 +0300

    first commit

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,14 +1,19 @@
 "use strict"
 function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
+  let arr = [];
+  let D = b**2 - 4*a*c;
+  let x1; x2;
+
+  if (D < 0) {
+    arr = [];
+  } else if (D == 0) {
+    x1 = -b / (2 * a)
+  } else if (D > 0) {
+    x1 = (-b + Math.sqrt(d))/(2*a);
+    x2 = (-b - Math.sqrt(d) )/(2 * a)
+  }
+
   return arr; // array
 }
 
-function calculateTotalMortgage(percent, contribution, amount, date) {
-  let totalAmount;
-
-  // код для задачи №2 писать здесь
-
-  return totalAmount;
-}
+solveEquation(1, 2, 3);
