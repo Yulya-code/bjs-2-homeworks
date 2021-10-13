35086cd3f01caa2b34485746d9106668e911b5f4
Yuliya Trunova <trunovanova@gmail.com>
2021-10-13 18:01:18 +0300

    task2 added

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -10,9 +10,7 @@ if (arr1.length === arr2.length && arr1.every((item, idx) => item === arr2[idx])
 
 
 function advancedFilter(arr) {
-  let resultArr;
-
-  // Ваш код
+  let resultArr = arr.filter((item => item > 0 && item % 3 === 0)). map((item => item * 10));
 
   return resultArr; // array
 }
