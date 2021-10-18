a0faf9e5828b36984079575ed14551acbb279826
Yuliya Trunova <trunovanova@gmail.com>
2021-10-18 22:41:43 +0300

    else deleted

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -13,17 +13,13 @@ Student.prototype.setSubject = function (subjectName) {
 Student.prototype.addMark = function (mark) {
 if(this.marks === undefined){ 
   this.marks = []; 
-  } else {
-   this.marks.push(mark);
-  }
+  };
 }
 
 Student.prototype.addMarks = function (...marks) {
 if(this.marks === undefined){ 
   this.marks = []; 
-  } else {
-  this.marks.push(...marks);
-  }
+  };
 }
 
   Student.prototype.getAverage = function () {
